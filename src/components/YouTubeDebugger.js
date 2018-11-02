9a0651943a89a908b3a6faca308f37909fccdca0
Nate Miller <natekmiller96@gmail.com>
2018-11-02 20:48:12 +0000

    automatically backed up by learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -2,6 +2,22 @@ import React, {Component} from 'react';
 
 
 export default class YouTubeDebugger extends Component {
+  constructor() {
+    super();
+ 
+    
+    this.state = {
+      errors: 0,
+      user: null,
+      settings:
+      {
+        birate: 8,
+        video: {
+          reso
+        }
+      }
+    };
+  }
   typing = () => {console.log('Entering password...')};
   render () {
     return(
