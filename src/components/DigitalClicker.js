19a0bc351ad88057ef973b35ba5567310b1d475f
Nate Miller <natekmiller96@gmail.com>
2018-11-02 20:36:26 +0000

    automatically backed up by learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,10 @@
-// Code DigitalClicker Component Here
+import React, {Component} from 'react';
+
+
+export default class DigitalClicker extends Component {
+  typing = () => {console.log('Entering password...')};
+  render () {
+    return(
+  <input onKeyUp={this.typing} type="password" />)
+  }
+}
